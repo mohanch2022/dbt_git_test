2eec8f9b743f3c6e5422052417a81864e6760981
chandra mohan <kvchandramohan@gmail.com>
2025-09-20 02:36:50 +0000

    tes

--- models/Myview_test.sql
+++ models/Myview_test.sql
@@ -1,5 +1,8 @@
-with dbt_test as(
 
-    select Region, Country,Item_Type from `global-env-411803.mydataset.test`
+
+with Myview_test as(
+
+    select dno,dname, CURRENT_TIMESTAMP() as Insert_timestamp from `global-env-411803.mydataset.dept`
 )
-select * from dbt_test
+select * from Myview_test
+
---
+++ models/ref_test.sql
@@ -0,0 +1,6 @@
+{{config(materialized= 'view')}}
+
+with dbt_test as(
+
+    select Region, Country,Item_Type from {{ref('Myview_test')}}
+)
